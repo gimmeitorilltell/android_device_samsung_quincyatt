0696ae986dd3f97673bcd92f3c53efd793e71b34
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-06 10:44:42 -0500

    This should have be changed to build pac. We will use cm-12.0 branches and apply changes for unsupported devices in the vendor/pac source.

--- cm.mk
+++ cm.mk
@@ -1,9 +1,12 @@
 $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 
-# Inherit some common pac stuff.
-$(call inherit-product, vendor/pac/config/pac_common.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common cm stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.2/JZO54K/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.2 JZO54K I717UCMD3 release-keys"
 
-PRODUCT_NAME := pac_quincyatt
+PRODUCT_NAME := cm_quincyatt
 PRODUCT_DEVICE := quincyatt
