a0a61f0c3a654c378f77afbc73595f65704bc799
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-05 22:01:26 -0500

    remove & replace for PAC build

--- cm.mk
+++ cm.mk
@@ -1,10 +1,7 @@
 $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/pac/config/nfc_enhanced.mk)
-
 # Inherit some common pac stuff.
-$(call inherit-product, vendor/pac/config/common_full_phone.mk)
+$(call inherit-product, vendor/pac/config/pac_common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.2/JZO54K/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.2 JZO54K I717UCMD3 release-keys"
 
