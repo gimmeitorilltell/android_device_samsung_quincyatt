SlimDev/android_device_samsung_t769

Fix mounting partitions in TWRP: Use a twrp.fstab

BoardConfig.mk
@@ -41,6 +41,7 @@ BOARD_NEEDS_MEMORYHEAPPMEM := true
 # TWRP
 DEVICE_RESOLUTION := 480x800
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := device/samsung/t769/twrp.fstab
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"

gimmeitorilltell/android_device_samsung_quincyatt

BoardConfig.mk
@@ -53,6 +53,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/quincyatt/bluetoot
 # TWRP
 DEVICE_RESOLUTION := 800x1280
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := device/samsung/quincyatt/twrp.fstab
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"